2ef6867a45757c3b6b0c9a1e8c73c018d914a790
mleila <mohamed.laila@mail.mcgill.ca>
2018-09-16 21:16:28 +0000

    add preprocedding step to the read data function

--- data.py
+++ data.py
@@ -9,11 +9,13 @@ traintypes = {'fare_amount': 'float32',
           'passenger_count': 'uint8'}
 
 
-def read_training_data(pth, chunksize=5000000):
+def read_training_data(pth, preprocessor, chunksize=5000000):
     cols = list(traintypes.keys())
     df_list = []
     for df_chunk in pd.read_csv(pth, usecols=cols, dtype=traintypes, chunksize=chunksize):
         df_chunk['pickup_datetime'] = df_chunk['pickup_datetime'].str.slice(0, 16)
         df_chunk['pickup_datetime'] = pd.to_datetime(df_chunk['pickup_datetime'], utc=True, format='%Y-%m-%d %H:%M')
+        preprocessor = preprocessor.fit(df_chunk)
+        df_chunk = preprocessor.transform(df_chunk)
         df_list.append(df_chunk)
     return pd.concat(df_list)
